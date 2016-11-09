21195cd5d04f5539357cd2535617b02d65b7fc90
houseofkraft <littlelettere@gmail.com>
2016-11-09 15:52:02 -0500

    Update codestudio.lua

--- codestudio.lua
+++ codestudio.lua
@@ -23,5 +23,23 @@ if fs.exists("codestudio") == false then
     file.close()
   end
 end
-  
-     
+
+-- Load images
+main = paintutils.loadImage("main")
+
+-- Functions
+local function centerText(text)
+  local x,y = term.getSize()
+  local x2,y2 = term.getCursorPos()
+  term.setCursorPos(math.round((x / 2) - (text:len() / 2)), y2)
+  write(text)
+end
+
+-- Init
+term.clear()
+term.setCursorPos(1,1)
+paintutils.drawImage(main, 1, 1)
+os.sleep(1)
+term.clear()
+term.setCursorPos(1,1)
+centerText("HouseCode")
